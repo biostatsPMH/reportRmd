84e3a4e584689de3591e3ab40e63e7c4825c0df8
Lisa Avery <lisa.avery@uhnresearch.ca>
2022-06-14 11:04:39 -0400

    option to set showN=FALSE in warning for crr and mising data

--- R/main.R
+++ R/main.R
@@ -1291,7 +1291,7 @@ mvsum <- function (model, data, digits=2, showN = F, markup = T, sanitize = T, n
     } else if (model$n==nrow(stats::na.omit(data))) {
       data <- stats::na.omit(data)
     } else {
-      if (showN) stop('For crr models, the supplied data frame can contain only non-missing data.\n Run na.omit() on a data frame containing only model variables.')
+      if (showN) stop('For crr models, the supplied data frame can contain only non-missing data.\n Either set showN = FALSE or run na.omit() on a data frame containing only model variables.')
     }
   } else if (type=='coxph'){
     if (missing(data)) stop("Data can not be derived from model, data argument must be supplied.")
